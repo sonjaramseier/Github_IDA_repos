5d7fa17f07596975a096846bb2cc1dc4626713cc
sonjaramseier <sonja.ramseier@stud.hslu.ch>
2022-05-12 10:58:56 +0200

    .

--- Sonstige_Uebungen/2022.05.12_test-fetch/assets/js/script.js
+++ Sonstige_Uebungen/2022.05.12_test-fetch/assets/js/script.js
@@ -16,7 +16,16 @@ fetch(base_url + query) //url anfragen für Daten
 
 
 function build_facts(data) {
+    let cat_facts_div = document.querySelector('.cats-facts')
     console.log(data)
+    for (let i = 0; i < data.length; i++) {
+        let data_point = data[i]
+            //console.log(data_point)
+        console.log(data_point['text']) //nur Text anzeigen
+        let cat_fact = document.createElement('div') //virtuelles div in javascript bauen
+        cat_fact.textContent = data_point['text'] //text hinzufügen
+        cat_facts_div.appendChild(cat_fact) //zu html element hinzufügen, das weiter oben eingefügt wurde
+    }
 }
 
 // https://cataas.com/cat
