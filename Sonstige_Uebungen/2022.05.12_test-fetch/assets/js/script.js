e445ba595e52faf7a4fe5ae5f9465a0747de1537
sonjaramseier <sonja.ramseier@stud.hslu.ch>
2022-05-12 13:18:16 +0200

    -

--- Sonstige_Uebungen/2022.05.12_test-fetch/assets/js/script.js
+++ Sonstige_Uebungen/2022.05.12_test-fetch/assets/js/script.js
@@ -1,19 +1,20 @@
-let base_url = 'https://cat-fact.herokuapp.com'
+function request_cats_facts() {
+    let base_url = 'https://cat-fact.herokuapp.com'
 
-let query = '/facts'
+    let query = '/facts'
 
-fetch(base_url + query) //url anfragen für Daten
-    .then(function(response) { //auf response warten
-        return response.json() //format
-    })
-    .then(function(data) { //was tun mit information
-        build_facts(data)
+    fetch(base_url + query) //url anfragen für Daten
+        .then(function(response) { //auf response warten
+            return response.json() //format
+        })
+        .then(function(data) { //was tun mit information
+            build_facts(data)
 
-    })
-    .catch(function(error) {
-        console.log(error)
-    })
-
+        })
+        .catch(function(error) {
+            console.log(error)
+        })
+}
 
 function build_facts(data) {
     let cat_facts_div = document.querySelector('.cats-facts')
@@ -27,5 +28,44 @@ function build_facts(data) {
         cat_facts_div.appendChild(cat_fact) //zu html element hinzufügen, das weiter oben eingefügt wurde
     }
 }
+
+function request_coffee() {
+    let base_url = 'https://coffee.alexflipnote.dev'
+    let query = '/random.json'
+
+    fetch(base_url + query)
+        .then(
+            function(response) {
+                return response.json()
+            }
+        )
+        .then(
+            function(data) {
+                build_coffee(data)
+            }
+        )
+        .catch(
+            function(error) {
+                console.log(error)
+            }
+        )
+}
+
+function build_coffee(data) {
+    console.log(data['file'])
+    let coffee_div = document.querySelector('.coffee')
+    let coffee_img = document.createElement('img')
+    coffee_img.src = data['file']
+    coffee_div.appendChild(coffee_img)
+}
+
+window.onload = function() {
+    let coffee_button = document.querySelector('.kafi')
+    console.log(coffee_button)
+
+    coffee_button.addEventListener('click', request_coffee) //event listener zum button hinzufügen, hier click
+}
+
+
 
 // https://cataas.com/cat
